2e7efbc1dbcaeeeb41349488b68324fc3971fd1a
Jaster Starý <jasterstary@gmail.com>
2024-03-06 20:14:12 +0100

    fix

--- pkg/install.sh
+++ pkg/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# npm install --save ...
 npm install apexcharts --save
 npm install luxon --save
 
 mkdir 'luxon-build';
-webpack --config ../../webpack.config.cjs --entry="./build.js" --output-path="./luxon-build/"
+webpack --entry="./build.js" --output-path="./luxon-build/"
