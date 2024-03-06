fed25d88ad920a18b086787df6a31d383498a192
Jaster Starý <jasterstary@gmail.com>
2024-03-06 22:49:45 +0100

    fix

--- pkg/dependency.php
+++ pkg/dependency.php
@@ -3,7 +3,8 @@
 return [
   'js' => [
     NODE . '/apexcharts/dist/apexcharts.js',
-    '/apexcharts/luxon-build/main.js',
+    NODE . '/luxon/build/global/luxon.js',
+    //'/apexcharts/luxon-build/main.js',
     '/apexcharts/plugin.js',
   ],
   'css' => [
--- pkg/install.sh
+++ pkg/install.sh
@@ -3,5 +3,5 @@
 npm install apexcharts --save
 npm install luxon --save
 
-mkdir 'luxon-build';
-webpack --entry="./build.js" --output-path="./luxon-build/"
+#mkdir 'luxon-build';
+#webpack --entry="./build.js" --output-path="./luxon-build/"
