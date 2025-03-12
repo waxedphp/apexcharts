630bd6150097d94857a215fc37c3a6a2d8329fa0
Andy <andy@zoltan.zde>
2025-03-12 09:52:17 +0100

    Updated 2025-03-12

--- pkg/plugin.js
+++ pkg/plugin.js
@@ -21,8 +21,8 @@
     const unitsDecimal = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     function formatBytesDecimal(x){
       let l = 0, n = parseInt(x, 10) || 0;
-      while(n >= 1024 && ++l){
-          n = n/1024;
+      while(n >= 1000 && ++l){
+          n = n/1000;
       }
       return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + unitsDecimal[l]);
     }
